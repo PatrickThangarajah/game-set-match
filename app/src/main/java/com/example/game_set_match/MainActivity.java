8eef260a66f79e6ffa907136f681757bc6fbefc1
Arseni Borisovs <arsenybor@gmail.com>
2020-01-11 15:50:47 -0800

    Initial commit

---
+++ app/src/main/java/com/example/game_set_match/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.game_set_match;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
