7de1d4149cd9d7bb4d9204253f657d532d0dd713
PKPMuffMan <stefanungurean@hotmail.com>
2020-01-11 19:28:07 -0800

    add game file

--- app/src/main/java/com/example/game_set_match/Game.java
+++ app/src/main/java/com/example/game_set_match/Game.java
@@ -1,4 +1,5 @@
 package com.example.game_set_match;
+implementation 'com.google.firebase:firebase-database:16.0.6';
 
 import java.time.*;
 import java.time.LocalDateTime;
@@ -17,12 +18,14 @@ public class Game {
     }
 
     public void StartGame(newUser player1, newUser player2) {
+        private game_set_match_nosql = FirebaseDatabase.getInstance();
         this.gameId = 1; // generate game ID
         this.StartTime = LocalDateTime.now();
         this.player1 = player1;
         this.player2 = player2;
         this.winner = new newUser();
         this.GameOver = false;
+        mDatabase.child("StartTime").child(userId).setValue(user);
 
         // figure out how to store this into a new document on firebase;
         // this.gameId;
