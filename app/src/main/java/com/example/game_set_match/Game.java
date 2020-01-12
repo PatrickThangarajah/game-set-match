a734f4c4f7798296824da4a75efd5bedf14c7842
jacebedo <jacebedo@sfu.ca>
2020-01-11 19:51:55 -0800

    add EndGame logic

--- app/src/main/java/com/example/game_set_match/Game.java
+++ app/src/main/java/com/example/game_set_match/Game.java
@@ -1,6 +1,14 @@
 package com.example.game_set_match;
 
+import android.nfc.Tag;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -8,7 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Game {
-    private int gameId;
+    private int GameId;
     private Date StartTime;
     private Date endTime;
     private newUser player1;
@@ -21,7 +29,7 @@ public class Game {
     }
 
     public void StartGame(newUser player1, newUser player2) {
-        this.gameId = 1; // generate game ID
+        this.GameId = 1; // generate game ID
         this.StartTime = Calendar.getInstance().getTime();
         this.player1 = player1;
         this.player2 = player2;
@@ -38,18 +46,39 @@ public class Game {
     }
 
     public void EndGame(int gid) {
-        Map<String, Object> params = new HashMap<>();
+        final Map<String, Object> params = new HashMap<>();
 
         params.put("GameOver",true);
         params.put("EndTime",Calendar.getInstance().getTime());
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        //db.collection("Active Games")
+        db.collection("Active_Games")
+                .document(Integer.toString(gid))
+                .set(params, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        this.GameOver = params.get("GameOver");
+                        this.EndTime = params.get("EndTime");
+                        // go to winner screen
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception ex) {
+                        Log.e("error",ex.getMessage());
+                    }
+                });
 
     }
 
     public void SetWinner(int gameId,String winner) {
-
+        // TODO
+        // winner = winner's username
+        // game = game id
+        // algorithm:
+        //  1. get game from firestore
+        //  2. get winning player based on winner username,
     }
 }
